eafb3fa981630a56f18940656b74f5df2e26de38
david@dehusky.co.uk <Denise78!>
2021-03-26 04:45:51 +0000

    update from github attempt 2

--- update_from_github.sh
+++ update_from_github.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/python
 
-import git
-
 cd /opt/PPC_WB_Tester
-g=git.cmd.Git('https://github.com/dehusky/PPCWaterBlock')
+pip install --upgrade git+https://github.com/dehusky/PPCWaterBlock
 
-g.pull()
+
 #sudo git pull origin master
