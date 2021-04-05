53dfe6c2a7b78b27fc2a10fac73c0fbda9dcfdec
david@dehusky.co.uk <Denise78!>
2021-04-05 19:30:35 +0100

    Add Desktop Entry

--- update_from_github.sh
+++ update_from_github.sh
@@ -10,7 +10,7 @@ FILE=/home/pi/Desktop/WaterBlockTimer
 if test -f "$FILE"; then
     echo "$FILE exists."
 else
-    sudo cp /opt/PPCWaterBlock/Water Block Timer /home/pi/Desktop/
+    sudo cp /opt/PPCWaterBlock/WaterBlockTimer /home/pi/Desktop/
 fi
 
 sh run.sh
