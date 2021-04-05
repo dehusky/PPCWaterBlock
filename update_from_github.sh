4af0d31f9d2fc43ced58418997295f68788dafb9
david@dehusky.co.uk <Denise78!>
2021-04-05 19:20:45 +0100

    Add Desktop Entry

--- update_from_github.sh
+++ update_from_github.sh
@@ -5,4 +5,12 @@ cd /opt/PPC_WB_Tester
 #curl -v -H "Authorization:103d0193b48df2634e1612e6a566d68a791e5e67" https://api.github.com/dehusky/PPCWaterBlock
 
 sudo git pull origin master
+
+FILE=/home/pi/Desktop/Water Block Timer
+if test -f "$FILE"; then
+    echo "$FILE exists."
+else
+    cp /opt/PPCWaterBlock/Water Block Timer /home/pi/Desktop/
+fi
+
 sh run.sh
