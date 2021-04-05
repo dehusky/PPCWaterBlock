d71a96186600d4d74e5b669048d6867725c8027a
david@dehusky.co.uk <Denise78!>
2021-04-05 19:33:42 +0100

    Add Desktop Entry

--- update_from_github.sh
+++ update_from_github.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/python
 
-cd /opt/PPC_WB_Tester
-#pip install --upgrade git+https://github.com/dehusky/PPCWaterBlock/*
-#curl -v -H "Authorization:103d0193b48df2634e1612e6a566d68a791e5e67" https://api.github.com/dehusky/PPCWaterBlock
+cd /opt/PPCWaterBlock
 
 sudo git pull origin master
 
